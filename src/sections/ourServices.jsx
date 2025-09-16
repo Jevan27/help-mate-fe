ecf296036fa16bad51e629d4c471f6783fdaa756
Jevan27 <campillosjevanlloyd27@gmail.com>
2025-09-16 13:57:25 +0800

    feat(ui): create Our Services section with reusable dividers and background
    - Built initial Our Services section with structure and styling
    - Added reusable dividers
    - Implemented background design

--- src/sections/ourServices.jsx
+++ src/sections/ourServices.jsx
@@ -1,16 +1,40 @@
 import Wave from "../assets/svg/wave1.svg?react";
+import { Dividers } from "../components/ui/divider";
+import BgSvg from "../components/ui/bgblob";
+import img1 from "../assets/images/education.png"
+import img2 from "../assets/images/home.png"
+import img3 from "../assets/images/lifestyle.png"
+import img4 from "../assets/images/tech.png"
 
 export default function OurServices() {
+  const services = [
+    { image: img1, text: "Home & Personal Assistance" },
+    { image: img2, text: "Tech & Digital Assistance" },
+    { image: img3, text: "Lifestyle & Wellness" },
+    { image: img4, text: "Education & Learning Support" },
+  ];
   return (
-    <section className="relative min-h-screen">
+    <section className="relative min-h-screen overflow-hidden">
       {/* SVG as inline React component */}
-      <Wave className="absolute inset-0 w-full h-full object-cover" preserveAspectRatio="none" />
-
-
+      <BgSvg/>
       {/* Content above it */}
-      <div className="relative z-10 flex items-center justify-center h-full">
-        <h1 className="text-white text-3xl">Our Services</h1>
+       <h1 className="relative z-10 text-center text-6xl font-bold text-sky-300 mb-12 font-nunito pt-20">
+        Our Services
+      </h1>
+
+        <div className="relative z-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+        {services.map((service, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center p-10 "
+          >
+            <img src={service.image} alt={service.text} className="mb-4 w-48 h-48 object-cover" />
+            <p className="font-inter text-2xl text-center font-bold text-sky-500">{service.text}</p>
+          </div>
+        ))}
       </div>
+      {Dividers.wave}
+
     </section>
   );
 }
